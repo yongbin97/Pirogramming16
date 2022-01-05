ff313091339b6fdbef148c27c67038588caec228
yongbin <jinnem9025@daum.net>
2022-01-05 12:55:14 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,38 +1,45 @@
 num = 0
 
-flag=True
+while num < 31:
+    flag=True
 
-while(flag):
-    try:
-        inputValue = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
-        if inputValue <1 or inputValue>3:
-            print("1, 2, 3 중 하나를 입력하세요.")
+    while(flag):
+        try:
+            inputValue = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
+            if inputValue <1 or inputValue>3:
+                print("1, 2, 3 중 하나를 입력하세요.")
+            else:
+                flag = False
+        
+        except:
+            print("정수를 입력하세요.")
+
+    for i in range(inputValue):
+        if num + 1 <= 31:
+            print("playerA: {}".format(num + 1))
+            num += 1
         else:
-            flag = False
-            print(inputValue)
-    
-    except:
-        print("정수를 입력하세요.")
+            break
 
-for i in range(inputValue):
-    print("playerA: {}".format(num + 1))
-    num += 1
+    flag =True
+    while(flag):
+        try:
+            inputValue = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
+            if inputValue <1 or inputValue>3:
+                print("1, 2, 3 중 하나를 입력하세요.")
+            else:
+                flag = False
+        
+        except:
+            print("정수를 입력하세요.")
 
-flag =True
-while(flag):
-    try:
-        inputValue = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
-        if inputValue <1 or inputValue>3:
-            print("1, 2, 3 중 하나를 입력하세요.")
+    for i in range(inputValue):
+        if num + 1 <= 31:
+            print("playeB: {}".format(num + 1))
+            num += 1
         else:
-            flag = False
-            print(inputValue)
+            break
     
-    except:
-        print("정수를 입력하세요.")
 
-for i in range(inputValue):
-    print("playerB: {}".format(num + 1))
-    num += 1
 
 
