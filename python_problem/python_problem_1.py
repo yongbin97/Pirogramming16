89f791c24ea98ac529a185fdecad8496a443dd4c
yongbin <jinnem9025@daum.net>
2022-01-05 12:47:38 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -15,4 +15,24 @@ while(flag):
         print("정수를 입력하세요.")
 
 for i in range(inputValue):
-    print("playerA: {}".format(i+1))
+    print("playerA: {}".format(num + 1))
+    num += 1
+
+flag =True
+while(flag):
+    try:
+        inputValue = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
+        if inputValue <1 or inputValue>3:
+            print("1, 2, 3 중 하나를 입력하세요.")
+        else:
+            flag = False
+            print(inputValue)
+    
+    except:
+        print("정수를 입력하세요.")
+
+for i in range(inputValue):
+    print("playerB: {}".format(num + 1))
+    num += 1
+
+
