581392fcd25dc60c8bde0e26e50f5eabcf38366f
yongbin <jinnem9025@daum.net>
2022-01-05 13:14:57 +0900

    [FIX] 1-6  오류수정

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,45 +1,59 @@
+class wrongInput(Exception):
+    def __init__(self):
+        super().__init__('1, 2, 3 중 하나를 입력하세요.')
+
 num = 0
+winner = 0
+flag=True
 
 while num < 31:
-    flag=True
-
     while(flag):
         try:
             inputValue = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
-            if inputValue <1 or inputValue>3:
-                print("1, 2, 3 중 하나를 입력하세요.")
-            else:
-                flag = False
-        
-        except:
+            if inputValue < 1 or inputValue > 3:
+                raise wrongInput
+            break
+
+        except ValueError:
             print("정수를 입력하세요.")
+
+        except wrongInput:
+            print("1, 2, 3 중 하나를 입력하세요.")
 
     for i in range(inputValue):
         if num + 1 <= 31:
             print("playerA: {}".format(num + 1))
             num += 1
         else:
+            winner = 1
+            flag=False
             break
-
-    flag =True
+        
     while(flag):
         try:
             inputValue = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
-            if inputValue <1 or inputValue>3:
-                print("1, 2, 3 중 하나를 입력하세요.")
-            else:
-                flag = False
-        
-        except:
+            if inputValue < 1 or inputValue > 3:
+                raise wrongInput
+            break
+
+        except ValueError:
             print("정수를 입력하세요.")
+            
+        except wrongInput:
+            print("1, 2, 3 중 하나를 입력하세요.")
 
     for i in range(inputValue):
         if num + 1 <= 31:
             print("playeB: {}".format(num + 1))
             num += 1
         else:
+            winner = 2 
+            flag=False
             break
     
-
+# if winner == 1:
+#     print("PlayerA win")
+# else:
+#     print("PlayerB win")
 
 
