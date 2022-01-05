14832380d1980c5e8e05e8749cd423a7f98329bb
yongbin <jinnem9025@daum.net>
2022-01-05 13:29:27 +0900

    1-7

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -3,11 +3,9 @@ class wrongInput(Exception):
         super().__init__('1, 2, 3 중 하나를 입력하세요.')
 
 num = 0
-winner = 0
-flag=True
 
 while num < 31:
-    while(flag):
+    while(num < 31):
         try:
             inputValue = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
             if inputValue < 1 or inputValue > 3:
@@ -20,19 +18,25 @@ while num < 31:
         except wrongInput:
             print("1, 2, 3 중 하나를 입력하세요.")
 
-    for i in range(inputValue):
-        if num + 1 <= 31:
-            print("playerA: {}".format(num + 1))
-            num += 1
-        else:
-            winner = 1
-            flag=False
-            break
-        
-    while(flag):
+    if (num <31):
+        for i in range(inputValue):
+            if num + 1 < 31:
+                print("playerA: {}".format(num + 1))
+                num += 1
+
+            elif num + 1 == 31:
+                print("playerA: {}".format(num + 1))
+                print("PlayerA win")
+                num += 1
+                break
+
+            else:
+                break
+    
+    while(num < 31):
         try:
-            inputValue = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
-            if inputValue < 1 or inputValue > 3:
+            inputValue2 = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
+            if inputValue2 < 1 or inputValue2 > 3:
                 raise wrongInput
             break
 
@@ -42,18 +46,18 @@ while num < 31:
         except wrongInput:
             print("1, 2, 3 중 하나를 입력하세요.")
 
-    for i in range(inputValue):
-        if num + 1 <= 31:
-            print("playeB: {}".format(num + 1))
-            num += 1
-        else:
-            winner = 2 
-            flag=False
-            break
-    
-# if winner == 1:
-#     print("PlayerA win")
-# else:
-#     print("PlayerB win")
+    if num < 31:
+        for i in range(inputValue2):
+            if num + 1 < 31:
+                print("playerB: {}".format(num + 1))
+                num += 1
 
+            elif num + 1 == 31:
+                print("playerB: {}".format(num + 1))
+                print("PlayerB win")
+                num += 1
+                break
+
+            else:
+                break
 
