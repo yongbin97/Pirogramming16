27218fbccfc547bf3402bb699cc62466378d76af
YongbinKim <jinnem9025@daum.net>
2022-01-12 01:39:01 +0900

    Django 템플릿

--- djangogirls/mysite/blog/views.py
+++ djangogirls/mysite/blog/views.py
@@ -5,7 +5,4 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
-
-
-Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
